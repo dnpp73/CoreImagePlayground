f61cfca8a541203a1bf9614760c513ab1b890b2d
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2021-10-11 03:29:41 +0900

    making UI ...

--- CoreImagePlayground/ViewController/CameraUIViewController.swift
+++ CoreImagePlayground/ViewController/CameraUIViewController.swift
@@ -5,6 +5,9 @@ import AVFoundation
 final class CameraUIViewController: UIViewController {
 
     @IBOutlet private var cameraFinderView: CameraFinderView!
+    @IBOutlet private var photoImageView: UIImageView!
+    @IBOutlet private var stillImageView: UIImageView!
+    @IBOutlet private var silentImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +27,23 @@ final class CameraUIViewController: UIViewController {
 
     @IBAction private func touchUpInsideScaleToFillButton(_ sender: UIButton) {
         cameraFinderView.contentMode = .scaleToFill
+        photoImageView.contentMode = .scaleToFill
+        stillImageView.contentMode = .scaleToFill
+        silentImageView.contentMode = .scaleToFill
     }
 
     @IBAction private func touchUpInsideAspectFitButton(_ sender: UIButton) {
         cameraFinderView.contentMode = .scaleAspectFit
+        photoImageView.contentMode = .scaleAspectFit
+        stillImageView.contentMode = .scaleAspectFit
+        silentImageView.contentMode = .scaleAspectFit
     }
 
     @IBAction private func touchUpInsideAspectFillButton(_ sender: UIButton) {
         cameraFinderView.contentMode = .scaleAspectFill
+        photoImageView.contentMode = .scaleAspectFill
+        stillImageView.contentMode = .scaleAspectFill
+        silentImageView.contentMode = .scaleAspectFill
     }
 
     @IBAction private func touchUpInsidePresetPhotoButton(_ sender: UIButton) {
@@ -42,8 +54,24 @@ final class CameraUIViewController: UIViewController {
         SimpleCamera.shared.setMovieMode()
     }
 
+    @IBAction private func touchUpInsidePhotoImageButton(_ sender: UIButton) {
+    }
+
+    @IBAction private func touchUpInsideStillImageButton(_ sender: UIButton) {
+        SimpleCamera.shared.captureStillImageAsynchronously { (image: UIImage?, metadata: [String: Any]?) -> Void in
+            self.stillImageView.image = image
+            if let image = image {
+                print(image)
+            }
+            if let metadata = metadata {
+                print(metadata)
+            }
+        }
+    }
+
     @IBAction private func touchUpInsideSilentImageButton(_ sender: UIButton) {
         SimpleCamera.shared.captureSilentImageAsynchronously { (image: UIImage?, metadata: [String: Any]?) -> Void in
+            self.silentImageView.image = image
             if let image = image {
                 print(image)
             }
